b7e88bdcc688f805c1b44d1a8bed8cc0be033550
Guang <jouyang19@proton.me>
2024-06-15 19:11:09 -0400

    complete lab

---
+++ lib/insert.sql
@@ -0,0 +1,15 @@
+INSERT INTO bears ( name, age, sex, color, temperament, alive) VALUES ( "Mr. Chocolate", 10, "M", "Royal Blue", "Cultured", TRUE);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 9, "M", "Beige", "rowdy", TRUE);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 8, "F", "Beige", "rowdy", TRUE);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 7, "M", "Beige", "rowdy", TRUE);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 6, "F", "blue", "calm", TRUE);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 5, "M", "red", "naughty", TRUE);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 4, "F", "red", "naughty", TRUE);
+
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 3, "M", "blue", "behaved", TRUE);
